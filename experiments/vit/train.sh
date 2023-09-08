64e70ea3159b5bf34916259907b8051195e9ca09
Lian ZiHAO <areebol@gpu017.scut-smil.cn>
2023-09-08 22:37:58 +0800

    Add experiments to control different experiment training

---
+++ experiments/vit/train.sh
@@ -0,0 +1,7 @@
+#! ~/bin/bash
+source /mnt/cephfs/home/areebol/miniconda3/bin/activate vit
+MODEL_NAME="vit"
+GPU=6
+TRAIN_FILE=../../main_ece.py
+echo "model: $MODEL_NAME, args: in GPU $GPU" 
+python $TRAIN_FILE --model-name $MODEL_NAME  --gpu $GPU 
